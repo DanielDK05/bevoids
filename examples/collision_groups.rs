80190f25d605a23ff2cf37b6fa95aa73e7f2fc4b
DanielDK05 <62153701+DanielDK05@users.noreply.github.com>
2024-02-20 03:05:13 +0100

    Added collision groups

---
+++ examples/collision_groups.rs
@@ -0,0 +1,86 @@
+use bevoids::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+const BOID_MIN_SPEED: f32 = 400.0;
+const BOID_MAX_SPEED: f32 = 1000.0;
+
+const BOID_COHESION: f32 = 0.1;
+const BOID_SEPARATION: f32 = 0.4;
+const BOID_ALIGNMENT: f32 = 0.14;
+const BOID_BORDER_TURN_STRENGTH: f32 = 200.0;
+
+const BOID_FOV: u32 = 240;
+const BOID_VIEW_RANGE: f32 = 120.0;
+const BOID_PROTECTED_RANGE: f32 = 60.0;
+
+const BORDER_WIDTH: f32 = 2400.0;
+const BORDER_HEIGHT: f32 = 1300.0;
+const BORDER_MARGIN: f32 = 400.0;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, BoidsPlugin))
+        .insert_resource(BoidsConfig {
+            space: BoidSpace::TwoDimensional,
+            debug: false,
+        })
+        .add_systems(Startup, setup)
+        .run()
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let mut cam_bundle = Camera2dBundle::default();
+    cam_bundle.projection.scale = 3.0;
+
+    commands.spawn(cam_bundle);
+
+    for x in -10..10 {
+        for y in -10..10 {
+            let spacer = 10.0;
+
+            let (group, material) = if x > 0 {
+                (
+                    BoidCollisionGroup::GROUP_1,
+                    materials.add(ColorMaterial::from(Color::RED)),
+                )
+            } else {
+                (
+                    BoidCollisionGroup::GROUP_2,
+                    materials.add(ColorMaterial::from(Color::CYAN)),
+                )
+            };
+
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: meshes.add(RegularPolygon::new(10.0, 3)).into(),
+                    material,
+                    transform: Transform::from_xyz(x as f32 * spacer, y as f32 * spacer, 0.0),
+                    ..default()
+                },
+                Boid::new(
+                    BoidSpeed::new(BOID_MIN_SPEED, BOID_MAX_SPEED),
+                    BoidTurningStrength::new(
+                        BOID_COHESION,
+                        BOID_SEPARATION,
+                        BOID_ALIGNMENT,
+                        BOID_BORDER_TURN_STRENGTH,
+                    ),
+                    BoidViewConfig::new(BOID_FOV, BOID_PROTECTED_RANGE, BOID_VIEW_RANGE),
+                ),
+                BoidBorder {
+                    left: Some((-BORDER_WIDTH / 2.0, BORDER_MARGIN)),
+                    right: Some((BORDER_WIDTH / 2.0, BORDER_MARGIN)),
+                    top: Some((BORDER_HEIGHT / 2.0, BORDER_MARGIN)),
+                    bottom: Some((-BORDER_HEIGHT / 2.0, BORDER_MARGIN)),
+                    front: None,
+                    back: None,
+                },
+                group,
+            ));
+        }
+    }
+}
--- src/boids.rs
+++ src/boids.rs
@@ -97,6 +97,89 @@ impl Boid {
     }
 }
 
+#[derive(Component, Clone, Copy)]
+pub struct BoidCollisionGroup(u32);
+
+bitflags::bitflags! {
+    impl BoidCollisionGroup: u32 {
+        /// The group n°1.
+        const GROUP_1 = 1 << 0;
+        /// The group n°2.
+        const GROUP_2 = 1 << 1;
+        /// The group n°3.
+        const GROUP_3 = 1 << 2;
+        /// The group n°4.
+        const GROUP_4 = 1 << 3;
+        /// The group n°5.
+        const GROUP_5 = 1 << 4;
+        /// The group n°6.
+        const GROUP_6 = 1 << 5;
+        /// The group n°7.
+        const GROUP_7 = 1 << 6;
+        /// The group n°8.
+        const GROUP_8 = 1 << 7;
+        /// The group n°9.
+        const GROUP_9 = 1 << 8;
+        /// The group n°10.
+        const GROUP_10 = 1 << 9;
+        /// The group n°11.
+        const GROUP_11 = 1 << 10;
+        /// The group n°12.
+        const GROUP_12 = 1 << 11;
+        /// The group n°13.
+        const GROUP_13 = 1 << 12;
+        /// The group n°14.
+        const GROUP_14 = 1 << 13;
+        /// The group n°15.
+        const GROUP_15 = 1 << 14;
+        /// The group n°16.
+        const GROUP_16 = 1 << 15;
+        /// The group n°17.
+        const GROUP_17 = 1 << 16;
+        /// The group n°18.
+        const GROUP_18 = 1 << 17;
+        /// The group n°19.
+        const GROUP_19 = 1 << 18;
+        /// The group n°20.
+        const GROUP_20 = 1 << 19;
+        /// The group n°21.
+        const GROUP_21 = 1 << 20;
+        /// The group n°22.
+        const GROUP_22 = 1 << 21;
+        /// The group n°23.
+        const GROUP_23 = 1 << 22;
+        /// The group n°24.
+        const GROUP_24 = 1 << 23;
+        /// The group n°25.
+        const GROUP_25 = 1 << 24;
+        /// The group n°26.
+        const GROUP_26 = 1 << 25;
+        /// The group n°27.
+        const GROUP_27 = 1 << 26;
+        /// The group n°28.
+        const GROUP_28 = 1 << 27;
+        /// The group n°29.
+        const GROUP_29 = 1 << 28;
+        /// The group n°30.
+        const GROUP_30 = 1 << 29;
+        /// The group n°31.
+        const GROUP_31 = 1 << 30;
+        /// The group n°32.
+        const GROUP_32 = 1 << 31;
+
+        /// All of the groups.
+        const ALL = u32::MAX;
+        /// None of the groups.
+        const NONE = 0;
+    }
+}
+
+impl Default for BoidCollisionGroup {
+    fn default() -> Self {
+        Self::ALL
+    }
+}
+
 #[derive(Component)]
 pub enum BoidCollider {
     Box(Vec3),
@@ -181,14 +264,27 @@ mod systems {
 
     pub(super) fn handle_boid_movement(
         time: Res<Time>,
-        mut boid_query: Query<(&mut Transform, &mut Boid, Option<&BoidBorder>, Entity)>,
+        mut boid_query: Query<(
+            &mut Transform,
+            &mut Boid,
+            Option<&BoidBorder>,
+            Option<&BoidCollisionGroup>,
+            Entity,
+        )>,
     ) {
         let boids = boid_query
             .iter()
-            .map(|(transform, boid, _, entity)| (*transform, *boid, entity))
+            .map(|(transform, boid, _, collision_group, entity)| {
+                (
+                    *transform,
+                    *boid,
+                    collision_group.and_then(|cg| Some(*cg)),
+                    entity,
+                )
+            })
             .collect::<Vec<_>>();
 
-        for (mut transform, mut boid, border, entity) in boid_query.iter_mut() {
+        for (mut transform, mut boid, border, collision_group, entity) in boid_query.iter_mut() {
             let mut movement_vector = Vec3::ZERO;
 
             let mut separation_vector = Vec3::ZERO;
@@ -196,9 +292,23 @@ mod systems {
             let mut average_position = Vec3::ZERO;
             let mut neighbouring_boids = 0;
 
-            for (neighbour_transform, neighbour_boid, neighbour_entity) in &boids {
+            for (
+                neighbour_transform,
+                neighbour_boid,
+                neighbour_collision_group,
+                neighbour_entity,
+            ) in &boids
+            {
                 if *neighbour_entity == entity {
                     continue;
+                }
+
+                if let Some(collision_group) = collision_group {
+                    if let Some(neighbour_collision_group) = neighbour_collision_group {
+                        if !collision_group.intersects(*neighbour_collision_group) {
+                            continue;
+                        }
+                    }
                 }
 
                 let distance = transform
